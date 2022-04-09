a26c84168d219cfed6be82173b7de62b9dfbf7c4
MAY-Y117 <103333720+MAY-Y117@users.noreply.github.com>
2022-04-09 13:36:25 -0700

    Update SQL Injection.java

--- SQL Injection.java
+++ SQL Injection.java
@@ -1,11 +1,29 @@
-// Get username from parameters
-String username = request.getParameter("username");
-// Create a statement from database connection
-Statement statement = connection.createStatement();  
-// Create unsafe query by concatenating user defined data with query string
-String query = "SELECT secret FROM Users WHERE (username = '" + username + "' AND NOT role = 'admin')";
-// ... OR ...
-// Insecurely format the query string using user defined data 
-String query = String.format("SELECT secret FROM Users WHERE (username = '%s' AND NOT role = 'admin')", username);
-// Execute query and return the results
-ResultSet result = statement.executeQuery(query);
+from django.contrib.auth.models import User
+
+User.objects.filter(username='admin').extra(
+    select={'test': 'secure'},
+    where=['secure'],
+    tables=['secure']
+)
+User.objects.filter(username='admin').extra({'test': 'secure'})
+User.objects.filter(username='admin').extra(select={'test': 'secure'})
+User.objects.filter(username='admin').extra(where=['secure'])
+
+User.objects.filter(username='admin').extra(dict(could_be='insecure'))
+User.objects.filter(username='admin').extra(select=dict(could_be='insecure'))
+query = '"username") AS "username", * FROM "auth_user" WHERE 1=1 OR "username"=? --'
+User.objects.filter(username='admin').extra(select={'test': query})
+User.objects.filter(username='admin').extra(select={'test': '%secure' % 'nos'})
+User.objects.filter(username='admin').extra(select={'test': '{}secure'.format('nos')})
+
+where_var = ['1=1) OR 1=1 AND (1=1']
+User.objects.filter(username='admin').extra(where=where_var)
+where_str = '1=1) OR 1=1 AND (1=1'
+User.objects.filter(username='admin').extra(where=[where_str])
+User.objects.filter(username='admin').extra(where=['%secure' % 'nos'])
+User.objects.filter(username='admin').extra(where=['{}secure'.format('no')])
+
+tables_var = ['django_content_type" WHERE "auth_user"."username"="admin']
+User.objects.all().extra(tables=tables_var).distinct()
+tables_str = 'django_content_type" WHERE "auth_user"."username"="admin'
+User.objects.all().extra(tables=[tables_str]).distinct()
